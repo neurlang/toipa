e2eda71540de8d5b7fb89ecc056cde8d6166139d
Your Name <you@example.com>
2023-10-28 20:32:10 +0100

    Sk2ipa

---
+++ sk2ipa/dict.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 code/run_dict.py --text=skfreq.txt  > skfreq.2023-10-28.txt
---
+++ sk2ipa/train.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+#NVIDIA_PATH="/usr/local/cuda-11.6/targets/x86_64-linux/lib/"
+#LD_LIBRARY_PATH=$NVIDIA_PATH python3 whatever.py
+export WANDB_MODE=offline
+
+python3 code/run_translation.py --model_name_or_path=t5-small --output_dir=out --train_file=dataset.txt --validation_file=evalset.txt  --source_lang sk --target_lang ipa --do_train --do_eval --overwrite_output_dir --num_train_epochs=16 --save_total_limit=3 --per_device_train_batch_size=32 --per_device_eval_batch_size=32 --fp16=True --use_fast_tokenizer --evaluation_strategy=epoch
