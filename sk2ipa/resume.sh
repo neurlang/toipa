e3baacb76712b38af1202eead7c274e6f512801b
Your Name <you@example.com>
2023-11-03 16:35:44 +0000

    Use vocab size

--- sk2ipa/resume.sh
+++ sk2ipa/resume.sh
@@ -10,4 +10,4 @@ ln -s -T "out/$model" my2.model
 
 export WANDB_MODE=offline
 
-python3 code/run_translation.py --model_name_or_path=my2.model --output_dir=out --train_file=dataset.txt --validation_file=evalset.txt  --source_lang sk --target_lang ipa --do_train --do_eval --overwrite_output_dir --num_train_epochs=16 --save_total_limit=3 --per_device_train_batch_size=32 --per_device_eval_batch_size=32 --fp16=True --use_fast_tokenizer --evaluation_strategy=epoch --resume=True
+python3 code/run_translation.py --vocab_size=62 --model_name_or_path=my2.model --output_dir=out --train_file=dataset.txt --validation_file=evalset.txt  --source_lang sk --target_lang ipa --do_train --do_eval --overwrite_output_dir --num_train_epochs=16 --save_total_limit=3 --per_device_train_batch_size=32 --per_device_eval_batch_size=32 --fp16=True --use_fast_tokenizer --evaluation_strategy=epoch --resume=True
--- sk2ipa/train.sh
+++ sk2ipa/train.sh
@@ -5,4 +5,4 @@
 #LD_LIBRARY_PATH=$NVIDIA_PATH python3 whatever.py
 export WANDB_MODE=offline
 
-python3 code/run_translation.py --model_name_or_path=t5-small --output_dir=out --train_file=dataset.txt --validation_file=evalset.txt  --source_lang sk --target_lang ipa --do_train --do_eval --overwrite_output_dir --num_train_epochs=16 --save_total_limit=3 --per_device_train_batch_size=32 --per_device_eval_batch_size=32 --fp16=True --use_fast_tokenizer --evaluation_strategy=epoch
+python3 code/run_translation.py --vocab_size=62 --model_name_or_path=t5-small --output_dir=out --train_file=dataset.txt --validation_file=evalset.txt  --source_lang sk --target_lang ipa --do_train --do_eval --overwrite_output_dir --num_train_epochs=16 --save_total_limit=3 --per_device_train_batch_size=32 --per_device_eval_batch_size=32 --fp16=True --use_fast_tokenizer --evaluation_strategy=epoch
