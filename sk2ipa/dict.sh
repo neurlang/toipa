5c5accf05309fe67055a17bdf8ef7f4fe23b9743
Your Name <you@example.com>
2023-11-03 16:36:09 +0000

    Current date

--- sk2ipa/dict.sh
+++ sk2ipa/dict.sh
@@ -4,4 +4,4 @@ model=`ls -1 -t out/ | grep checkpoint | head -n 1 | tr -d ':'`
 rm my2.model
 ln -s -T "out/$model" my2.model
 
-python3 code/run_dict.py --text=skfreq.txt  > skfreq.2023-10-29.txt
+python3 code/run_dict.py --text=skfreq.txt  > skfreq.2023-11-03.txt
